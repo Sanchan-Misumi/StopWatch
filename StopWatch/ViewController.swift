9581f0db54231f5c5da9b21d89fb62e344668776
Maho Misumi <maho.smile606@gmail.com>
2017-12-04 17:03:48 +0900

    これでいっか

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     var timer: Timer = Timer()
     
     var number: Int = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,6 @@ class ViewController: UIViewController {
 
     @IBAction func start () {
         if !timer.isValid {
-            
             timer = Timer.scheduledTimer(
                 timeInterval: 0.01,
                 target: self,
@@ -48,12 +48,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func lap () {
-        if timer.isValid, number == 0 {
-             label2.text = String(format: "%.2f", count)
+        if timer.isValid && number == 0 {
+            label2.text = String(format: "%.2f")
             number = number + 1
         } else if timer.isValid && number == 1{
-             label3.text = String(format: "%.2f", count)
-            number = number - 1
+            label3.text = String(format: "%.2f")
+             number = number - 1
         }
     }
     
